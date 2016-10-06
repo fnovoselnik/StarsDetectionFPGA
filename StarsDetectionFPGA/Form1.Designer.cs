0ab60716ef819fc182ada61cc42c8bd427970bcc
Filip Novoselnik <filip.novoselnik@gmail.com>
2016-10-06 16:27:15 +0200

    Final

---
+++ StarsDetectionFPGA/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace StarsDetectionFPGA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ports = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnUARTInit = new System.Windows.Forms.Button();
+            this.buttonUARTSend = new System.Windows.Forms.Button();
+            this.btn_DetectStars = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(40, 41);
+            this.buttonOpenFile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(101, 36);
+            this.buttonOpenFile.TabIndex = 2;
+            this.buttonOpenFile.Text = "Open image";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Ports:";
+            // 
+            // ports
+            // 
+            this.ports.FormattingEnabled = true;
+            this.ports.Location = new System.Drawing.Point(280, 48);
+            this.ports.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ports.Name = "ports";
+            this.ports.Size = new System.Drawing.Size(121, 24);
+            this.ports.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 112);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(481, 372);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnUARTInit
+            // 
+            this.btnUARTInit.Location = new System.Drawing.Point(573, 112);
+            this.btnUARTInit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnUARTInit.Name = "btnUARTInit";
+            this.btnUARTInit.Size = new System.Drawing.Size(109, 23);
+            this.btnUARTInit.TabIndex = 10;
+            this.btnUARTInit.Text = "UART init";
+            this.btnUARTInit.UseVisualStyleBackColor = true;
+            this.btnUARTInit.Click += new System.EventHandler(this.btnUARTInit_Click);
+            // 
+            // buttonUARTSend
+            // 
+            this.buttonUARTSend.Location = new System.Drawing.Point(573, 162);
+            this.buttonUARTSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonUARTSend.Name = "buttonUARTSend";
+            this.buttonUARTSend.Size = new System.Drawing.Size(109, 23);
+            this.buttonUARTSend.TabIndex = 11;
+            this.buttonUARTSend.Text = "UART send";
+            this.buttonUARTSend.UseVisualStyleBackColor = true;
+            this.buttonUARTSend.Click += new System.EventHandler(this.buttonUARTSend_Click);
+            // 
+            // btn_DetectStars
+            // 
+            this.btn_DetectStars.Location = new System.Drawing.Point(573, 212);
+            this.btn_DetectStars.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_DetectStars.Name = "btn_DetectStars";
+            this.btn_DetectStars.Size = new System.Drawing.Size(109, 28);
+            this.btn_DetectStars.TabIndex = 12;
+            this.btn_DetectStars.Text = "Mark Stars";
+            this.btn_DetectStars.UseVisualStyleBackColor = true;
+            this.btn_DetectStars.Click += new System.EventHandler(this.btn_DetectStars_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(200, 504);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(133, 28);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(582, 504);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 28);
+            this.buttonExit.TabIndex = 14;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 510);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Sending image:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 577);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btn_DetectStars);
+            this.Controls.Add(this.buttonUARTSend);
+            this.Controls.Add(this.btnUARTInit);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ports);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Name = "Form1";
+            this.Text = "StarsDetectionFPGA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ports;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnUARTInit;
+        private System.Windows.Forms.Button buttonUARTSend;
+        private System.Windows.Forms.Button btn_DetectStars;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ StarsDetectionFPGA/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Threading;
+
+namespace StarsDetectionFPGA
+{
+    public partial class Form1 : Form
+    {
+        //list with image values
+        public List<byte> pixelsSend = new List<byte>();
+        //list with x,y coordinates received from FPGA
+        public List<byte> pixelsIn = new List<byte>();
+
+        public byte RxIn;
+        public byte temp;
+
+        string[] ArrayComPortsNames = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Detection of available COM ports at application initialization
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int index = -1;
+            string ComPortName = null;
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            do
+            {
+                index += 1;
+                ports.Items.Add(ArrayComPortsNames[index]);
+            }
+
+            while (!((ArrayComPortsNames[index] == ComPortName)
+              || (index == ArrayComPortsNames.GetUpperBound(0))));
+            Array.Sort(ArrayComPortsNames);
+
+            // want to get first out
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+            ports.Text = ArrayComPortsNames[0];
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            // path to image
+            string fileToOpen;
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Choose image";
+            fileDialog.Filter = "Image file (*.jpg, *.jpeg, *.jpe, *.bmp, *.png) | *.jpg; *.jpeg; *.jpe; *.bmp; *.png";
+            fileDialog.InitialDirectory = @"Enter your initial directory with images";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // saving folder path to string
+                fileToOpen = fileDialog.FileName;
+                // create bitmap image using opened image
+                Bitmap inputImage = new Bitmap(fileToOpen, true);
+
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                // load image to pictureBox   
+                pictureBox1.Image = inputImage;
+
+                // getting image values, row by row
+                for (int y = 0; y < inputImage.Height; y++)
+                {
+                    for (int x = 0; x < inputImage.Width; x++)
+                    {
+                        // saving pixel values at location (x, y)
+                        Color pixelColor = inputImage.GetPixel(x, y);
+                        int avg = pixelColor.R;
+                        pixelsSend.Add((byte)avg);
+                    }
+                }
+            }
+            //Console.Write("\nImage opened!");
+        }
+
+        // this function is "listening" on serial port. If something is received at COM port via UART 
+        // SerialDataReceivedEventArgs event is activated and calls serialPort1_DataReceived function
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Console.Write("\nReceiving data!");
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+            RxIn = (byte)serialPort1.ReadByte();
+            // store values to list
+            pixelsIn.Add(RxIn);
+            // write received data to console
+            Console.WriteLine(RxIn);
+
+        }
+        // function which initalizes serial port connection and outputs adequate message if connection is successfully 
+        // established
+        private void btnUARTInit_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = ports.SelectedItem.ToString();
+            serialPort1.BaudRate = 9600;
+
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (serialPort1.IsOpen)
+            {
+                btnUARTInit.Enabled = false;
+                MessageBox.Show("UART connection at port " + serialPort1.PortName);
+            }    
+        }
+        // function which sends image values over UART to FPGA (Nexys3 in this case)
+        private void buttonUARTSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                buttonUARTSend.Enabled = false;
+            }
+            else return;
+
+            byte[] buff = new byte[1];
+            progressBar1.Value = 0;
+            int posto = pixelsSend.Count / 100;
+
+            for (int i = 0; i < pixelsSend.Count; ++i)
+            {
+                buff[0] = pixelsSend[i];
+                serialPort1.Write(buff, 0, 1);
+               
+                if (progressBar1.Value < 100)
+                    if ((i % posto) == 0) progressBar1.Value++;
+            }
+            pixelsIn.Clear();
+        }
+
+        // this function marks detected stars on image (coordinates received from FPGA after processing)
+        private void btn_DetectStars_Click(object sender, EventArgs e)
+        {
+            int n = 2;
+            int i = 0;
+            Image bmp = pictureBox1.Image;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                while (pixelsIn[i] != 0)
+                {
+                    int y = pixelsIn[i + 1];
+                    int x = pixelsIn[i];
+
+                    if (i < pixelsIn.Count() - 1)
+                        i += n;
+                    else
+                        break;
+
+                    int radius = 10;
+                    g.DrawEllipse(Pens.White, x - radius / 2, y - radius / 2, radius, radius);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        //  closing serial port connection and app
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            this.Close();
+        }
+    }
+}
